7c53eaf174c469f4c0eaf1d84d813e53fa6f7fdc
shehanminura <shehanminuradp@gmail.com>
2024-04-28 11:53:14 +0530

    add total and wickets

--- Exercise/function_exerise/cricket_match.py
+++ Exercise/function_exerise/cricket_match.py
@@ -1,5 +1,8 @@
 def start_game():
-    pass
+    global total_runs
+    total_runs=0
+    global wickets
+    wickets=0
 def overs_in_match():
     over=0
     while over<5 :
@@ -10,9 +13,29 @@ def overs_in_match():
 def ball_in_over(over):
     ball=0
     while ball<6:
-        print(f'over : {over}.{ball}')
+        print(f'Total {total_runs}/{wickets} over : {over}.{ball}')
+        score_off_balls()
+        
         ball+=1
+
+def score_off_balls():
+    global total_runs
+    global wickets
+    score =input(' Score :')        
+    if score.isdigit(): #validation1     
+        score=int(score)
+        #if 0<=score<5 or score == 6 :
+        if score in (0,1,2,3,4,6):
+            runs=score 
+            total_runs+=runs
+
+    elif score.lower()=="out":
+       wickets+=1
+                
+    
+
 
 def print_score():
     pass
+start_game()
 overs_in_match ()
